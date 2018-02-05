de0f70f7777a98bf79ede3f2ae06771767191956
Sarun <sarunluitel@gmail.com>
2018-02-04 21:03:37 -0700

    Initial commit

---
+++ src/Hw1.py
@@ -0,0 +1,30 @@
+# Implement the QuickSearch and Median-of-Medians algorithms discussed in class.  Add suitable code to keep track of how
+# many comparisons are done between pairs of array elements.  Then, test your code as follows:
+# Generate a randomly ordered array, A, of 100 distinct elements.  Run both of the QS and MoM algorithms on it to find
+# the 50th smallest element.  Specifically, run QS 100 times on array A, and store the comparison count for each run.
+# Run MoM on array A once (or twice, to make sure the count is the same both times) and store its comparison count.
+# Repeat the above experiment 100 times, storing all the comparison counts data.
+# Next, use this data to compute the following quantities:
+#
+# 1. Find the overall average number of comparisons needed by each algorithm.
+# 2. Determine in how many of the 10,000 trials of QS was the number of comparisons more than the number needed by MoM
+#  on the same input.
+# 3. For the 100 runs of MoM, find the minimum, 10th percentile, median, 90th percentile, and maximum number of comparisons done.
+# 4. Do the same for the 10K runs of QS.
+# Submit your code and its output on Learn.
+# There are some associated online questions that you will also need to answer.  This Survey is available just below,
+# and is a required part of the assignment.
+
+import random
+
+data = [x for x in range(0, 100)]
+random.shuffle(data)
+
+
+def quickSelect(data, k):
+
+    pivot = random.randint(0, len(data) - 1)
+    smallerSet, largerSet = [], []
+
+
+quickSelect(data, 2)
