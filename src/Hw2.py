d3edbc1cb515cec1b9b249f8bd7cb97399e4ccb0
Sarun <sarunluitel@gmail.com>
2018-04-07 16:11:07 -0600

    MoM implemented

--- src/Hw2.py
+++ src/Hw2.py
@@ -2,63 +2,72 @@ from scipy.optimize import minimize
 import numpy as np
 from Food import Food
 
+apple = Food('Apple', 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
+apple1 = Food('Apple1', 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
+chips = Food('chips', 1.0, 10.0, 10.0, 1.0, 1.0, 1.0, 1.0)
+chips1 = Food('chips1', 1.0, 10.0, 10.0, 1.0, 1.0, 1.0, 1.0)
+chips2 = Food('chips2', 1.0, 10.0, 10.0, 1.0, 1.0, 1.0, 1.0)
 
-def objective(foodList):
-    totalPrice = 0
-    for food in foodList:
-        totalPrice += food.price()
+FoodList = [apple, apple1, chips, chips1, chips2]
+
+
+def objective(foodNum):
+    totalPrice, index = 0, 0
+    for food in FoodList:
+        totalPrice += foodNum[index] * food.price()
+        index += 1
     return totalPrice
 
 
-def fat(FoodList):
-    fat = 0
+def fat(foodNum):
+    fat, index = 0, 0
     for food in FoodList:
-        fat += food.fat()
+        fat += foodNum[index] * food.fat()
     return 20 - fat
 
 
-def sodium(FoodList):
-    sodium = 0
+def sodium(foodNum):
+    sodium, index = 0, 0
     for food in FoodList:
-        sodium += food.sodium()
+        sodium += foodNum[index] * food.sodium()
+        index += 1
     return 2400 - sodium
 
 
-def vitC(FoodList):
-    vitC = 0
+def vitC(foodNum):
+    vitC, index = 0,0
     for food in FoodList:
-        vitC += food.vitC()
+        vitC += foodNum[index] * food.vitC()
+        index += 1
     return vitC - 90
 
 
-def vitA(FoodList):
-    vitA = 0
+def vitA(foodNum):
+    vitA, index = 0, 0
     for food in FoodList:
-        vitA += food.vitA()
+        vitA += foodNum[index] * food.vitA()
+        index += 1
     return vitA - 700
 
 
-def protien(FoodList):
-    protein = 0
+def protien(foodNum):
+    protein, index = 0, 0
     for food in FoodList:
-        protein += food.protein()
+        protein += foodNum[index] * food.protein()
+        index += 1
     return protein - 56
 
 
-def calories(FoodList):
-    calories = 0
+def calories(foodNum):
+    calories, index = 0,0
     for food in FoodList:
-        calories += food.calories()
+        calories += foodNum[index] * food.calories()
+        index += 1
     return 2000 - calories
 
 
 def main():
     #  Order of parameters (name, price, calories, fat, sodium, vitC, vitA, protein)
-    apple = Food('Apple', 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
-    apple1 = Food('Apple1', 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
-    chips = Food('chips', 1.0, 10.0, 10.0, 1.0, 1.0, 1.0, 1.0)
-    chips1 = Food('chips1', 1.0, 10.0, 10.0, 1.0, 1.0, 1.0, 1.0)
-    chips2 = Food('chips2', 1.0, 10.0, 10.0, 1.0, 1.0, 1.0, 1.0)
 
     con1 = {'type': 'eq', 'fun': calories}
     con2 = {'type': 'ineq', 'fun': fat}
@@ -68,12 +77,11 @@ def main():
     con6 = {'type': 'ineq', 'fun': protien}
 
     allcons = [con1, con2, con3, con4, con5, con6]
+    foodnum = np.array([1, 1, 1, 1, 1])
 
-    myFood = [apple, apple1, chips, chips1, chips2]
+    solution = minimize(objective, foodnum, method='SLSQP', constraints=allcons)
 
-    solution = minimize(objective, myFood,method='SLSQP', constraints=allcons)
-
-    print(protien(myFood))
+    print(solution)
 
 
 if __name__ == '__main__':
